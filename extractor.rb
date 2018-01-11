e1f2a490ba547ad00dc2e0c33e67f1e2736bb3bb
J. Austin Hughey <jah@jah.io>
2018-01-11 16:04:32 -0700

    works in an ideal case

--- extractor.rb
+++ extractor.rb
@@ -4,7 +4,10 @@
 #   USAGE: ./extractor.rb FILENAME
 #
 # Given a PDF file, extracts all the plain text data possible.
-# Flushes that plain text data into txt/SAMEFILE.txt.
+#
+# Exit Codes:
+#   1 - No filename argument given
+#   2 - Given PDF didn't have any extractable text
 #
 
 if ARGV.count < 1
@@ -15,6 +18,7 @@ end
 
 require 'pdf-reader'
 require 'pry'
+require 'pp'
 
 #
 # Hold the pages as an array here so we can assess if ANY of them
@@ -25,9 +29,26 @@ pdf = PDF::Reader.new(ARGV[0])
 @pages = pdf.pages
 unless @pages.any? { |p| p.text.length > 0 }
   puts "No text in the entire PDF."
-  exit 1
+  exit 2
 end
 
-pdf.pages.each do |p|
-  puts p.text
+@text = ''
+pdf.pages.each { |p| @text << p.text }
+
+#
+# Ideally there should be ONE entry on each line with ONE price
+# next to it. See if you can parse it that way...
+#
+@food = []
+@text.each_line do |x|
+  tmp = x.split(/(\d.*\.*\d.*)/)
+  yummy = Hash.new
+  yummy[:name]  = tmp[0].gsub(/\s/, '')
+  yummy[:price] = tmp[1].to_f
+  @food << yummy
 end
+
+#
+# Put results on the screen
+#
+pp @food
--- pdf/readme.md
+++ pdf/readme.md
@@ -1,6 +1,4 @@
 # /pdf
 
 This directory exists to house all PDF files for use in development/testing
-and manual usage on the command line. The PDFs themselves are `.gitignore`'d
-to preserve privacy; the directory, however, should exist and this `README`
-and the corresponding `.gitkeep` are here to tell you that.
+and manual usage on the command line.
